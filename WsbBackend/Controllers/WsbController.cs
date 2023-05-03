dc11aae577c6e9777f2c6bad2374f65fd673aaa5
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-05-03 17:27:32 +0100

    api call setup

---
+++ WsbBackend/Controllers/WsbController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WsbApiController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public WsbApiController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<object>> GetExternalData()
+        {
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+
+            // Replace this URL with the endpoint you want to call
+            string apiUrl = $"https://tradestie.com/api/v1/apps/reddit?date={today}";
+
+            // Make the GET request and receive the response
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+            // Read the response content as a string
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            // Return the response content as a JSON object
+            return new JsonResult(responseContent);
+        }
+    }
+}
---
+++ WsbBackend/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WsbBackend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
